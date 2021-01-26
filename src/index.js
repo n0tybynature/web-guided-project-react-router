0776d46fd60ca0146cfdc79e1563fe5ac94e0330
n0tybynature <74768895+n0tybynature@users.noreply.github.com>
2021-01-25 16:44:30 -0800

    donzo

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-
+import { BrowserRouter as Router } from "react-router-dom"
 // Importing the top-level component
 import App from './components/App'
 
@@ -9,8 +9,12 @@ import './styles.less'
 
 // 👉 STEP 1 - Import React Router's Router
 
+
+
 render(
   // Wrap the <App /> in a provider
+  <Router>
   <App />
+  </Router>
   , document.querySelector('#root')
 )
